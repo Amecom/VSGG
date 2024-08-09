eea901e1cacdbc23e701c1534920757c3a16c5c7
Amedeo <32417424+Amecom@users.noreply.github.com>
2024-08-09 19:32:06 +0200

    FEAT: update docs

--- contracts/interfaces/IVSGG.sol
+++ contracts/interfaces/IVSGG.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// See https://eips.ethereum.org/EIPS/eip-721 
 interface IERC721Receiver {
     function onERC721Received(
         address operator,
@@ -10,18 +11,16 @@ interface IERC721Receiver {
     ) external returns (bytes4);
 }
 
+// See https://eips.ethereum.org/EIPS/eip-165
 interface IERC165 {
-
     function supportsInterface(bytes4 interfaceId) external view returns (bool);
-
 }
 
-interface IERC721 is IERC165 {
-
+// See https://eips.ethereum.org/EIPS/eip-721
+interface IERC721 /* is IERC165 */ {
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-
     function balanceOf(address owner) external view returns (uint256 balance);
     function ownerOf(uint256 tokenId) external view returns (address owner);
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
@@ -31,18 +30,17 @@ interface IERC721 is IERC165 {
     function setApprovalForAll(address operator, bool approved) external;
     function getApproved(uint256 tokenId) external view returns (address operator);
     function isApprovedForAll(address owner, address operator) external view returns (bool);
-
 }
 
-interface IERC721Metadata is IERC721 {
-
+// See https://eips.ethereum.org/EIPS/eip-721 optional metadata extension 
+interface IERC721Metadata /* is IERC721 */ {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function tokenURI(uint256 tokenId) external view returns (string memory);
-
 }
 
-interface IVSGG is IERC721Metadata {
+// VSGG interface
+interface IVSGG is IERC165, IERC721, IERC721Metadata {
 
     event Consolidated(uint256 tokenId);
     event ContractFeeUpdated(uint256 newFee);
@@ -319,6 +317,7 @@ interface IVSGG is IERC721Metadata {
 }
 
 
+// VSGG Errors
 interface IVSGGSErrors {
 
     // ERC721 Errors
